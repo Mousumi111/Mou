8eea50486bb4930ef9700158aa549e00c08c2bf2
Hashin <Hashin>
2016-12-15 00:15:54 +0600

    This is new routine

---
+++ js/main.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+		$("#left-bg-yellow-js").mouseenter(function(){
+		$(".yellow-main").show();
+			
+	});
+		
+	$("#left-bg-yellow-js").mouseout(function(){
+		$(".yellow-main").hide();
+			
+	});
+		$("left-bg-blue-js").mouseenter(function(){
+		$(".blue-main").show();
+			
+	});
+		
+	$("#left-bg-blue-js").mouseout(function(){
+		$(".blue-main").hide();
+			
+	});
+	
+	
+		$("#left-bg-yellow-two-js").mouseenter(function(){
+		$(".yellow-two-main").show();
+			
+	});
+		
+	$("#left-bg-yellow-two-js").mouseout(function(){
+		$(".yellow-two-main").hide();
+			
+	});
+	
+		$("#left-bg-green-js").mouseenter(function(){
+		$(".green-main").show();
+			
+	});
+		
+	$("#left-bg-green-js").mouseout(function(){
+		$(".green-main").hide();
+			
+	});
+	
+		$("#blue_two_js").mouseenter(function(){
+		$(".main_blue_two").show();
+			
+	});
+		
+	$("#blue_two_js").mouseout(function(){
+		$(".main_blue_two").hide();
+			
+	});
+	
+		$("#yellow_two_js").mouseenter(function(){
+		$(".main_yellow_two").show();
+			
+	});
+		
+	$("#yellow_two_js").mouseout(function(){
+		$(".main_yellow_two").hide();
+			
+	});
+	
+		$("#green_js_two").mouseenter(function(){
+		$(".green_main_two").show();
+			
+	});
+		
+	$("#green_js_two").mouseout(function(){
+		$(".green_main_two").hide();
+			
+	});
+	
+	
+	
+	
+		$("#green_js-2").mouseenter(function(){
+		$(".green_main-2").show();
+			
+	});
+		
+	$("#green_js-2").mouseout(function(){
+		$(".green_main-2").hide();
+			
+	});
+	
+	
+		$("#yellow_js-2").mouseenter(function(){
+		$(".yellow_main-2").show();
+			
+	});
+		
+	$("#yellow_js-2").mouseout(function(){
+		$(".yellow_main-2").hide();
+			
+	});
+	
+	
+		$("#blue_js-2").mouseenter(function(){
+		$(".blue_main-2").show();
+			
+	});
+		
+	$("#blue_js-2").mouseout(function(){
+		$(".blue_main-2").hide();
+			
+	});
+	
+		$("#gray_js-2").mouseenter(function(){
+		$(".main_gray-2").show();
+			
+	});
+		
+	$("#gray_js-2").mouseout(function(){
+		$(".main_gray-2").hide();
+			
+	});
+	
+		$("#blue_two_js-2").mouseenter(function(){
+		$(".main_blue_two-2").show();
+			
+	});
+		
+	$("#blue_two_js-2").mouseout(function(){
+		$(".main_blue_two-2").hide();
+			
+	});
+	
+		$("#yellow_two_js-2").mouseenter(function(){
+		$(".main_yellow_two-2").show();
+			
+	});
+		
+	$("#yellow_two_js-2").mouseout(function(){
+		$(".main_yellow_two-2").hide();
+			
+	});
+	
+		$("#green_js_two-2").mouseenter(function(){
+		$(".green_main_two-2").show();
+			
+	});
+		
+	$("#green_js_two-2").mouseout(function(){
+		$(".green_main_two-2").hide();
+			
+	});
+
+});
